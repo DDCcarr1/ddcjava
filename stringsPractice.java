2d798b1154a939947bc28f6074047d03b3b2e31b
DDCcarr1 <ddccarr1@icloud.com>
2025-03-12 14:45:19 -0400

    stringsPractice template
    adding Strings Practice template to ddcjava repository

---
+++ stringsPractice.java
@@ -0,0 +1,7 @@
+public class stringsPractice{
+    public static void main(String[] args){
+        //Functions to do:
+        //length, toLowerCase, toUpperCase, trim, charAt, indexOf1
+        //indexOf2, subString1, subString2, equals, notEquals, concat
+    }//main
+}//stringsPractice
